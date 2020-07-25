4c62da0664065a250f3a5d343be34a14068827af
cvidal <chris.vidal10@gmail.com>
2020-07-25 12:54:09 -0300

    third commit

--- controllers/game.py
+++ controllers/game.py
@@ -1,20 +1,88 @@
 class Game():
 
     def __init__(self):
-      self.__rolls = list()
-      self.__current_roll = 0
+        """
+        Construct the object Game
+        """
+        self.__rolls = list()
+        self.__current_roll = 0
 
-    def roll(self, pins):
+    def roll(self, pins: int):
+        """
+        Method for a single roll.
+
+        Args:
+            pins (int): The amount of pins knocked down.
+        """
         self.__rolls.append(pins)
 
-    def score(self):
+    def score(self) -> int:
+        """
+        Methof for calculate the final score.
+
+        Returns:
+            int: Final score.
+        """
         score = 0
         roll_index = 0
         for frame in range(10):
-            if self.__rolls[roll_index]  + self.__rolls[roll_index + 1] == 10:
-                score += 10 + self.__rolls[roll_index + 2]
+            if self.__is_strike(roll_index):
+                score += self.__strike_bonus(roll_index)
+                roll_index += 1
+            elif self.__is_spare(roll_index):
+                score += self.__spare_bonus(roll_index)
                 roll_index += 2
             else:
-                score += self.__rolls[roll_index] + self.__rolls[roll_index + 1]
+                score += self.__sum_of_rolls(roll_index)
                 roll_index += 2
         return score
+
+    def __is_spare(self, roll_index: int) -> bool:
+        """
+        Args:
+            roll_index (int): Roll turn in a game.
+
+        Returns:
+            bools: True if is spare or false otherwise.
+        """
+        return self.__rolls[roll_index] + self.__rolls[roll_index + 1] == 10
+
+    def __is_strike(self, roll_index: int) -> bool:
+        """
+        Args:
+            roll_index (int): Roll turn in a game.
+
+        Returns:
+            bools: True if is strike or false otherwise.
+        """
+        return self.__rolls[roll_index] == 10
+
+    def __strike_bonus(self, roll_index: int) -> int:
+        """
+        Args:
+            roll_index (int): Roll turn in a game.
+
+        Returns:
+            int: The sum of a complete strike 10 + bonus in a frame.
+        """
+        return 10 + self.__rolls[roll_index + 1] + self.__rolls[roll_index + 2]
+
+    def __spare_bonus(self, roll_index: int) -> int:
+        """
+        Args:
+            roll_index (int): Roll turn in a game.
+
+        Returns:
+            int: The sum of a complete spare 10 + bonus in a frame.
+        """
+        return 10 + self.__rolls[roll_index + 2]
+
+    def __sum_of_rolls(self, roll_index: int) -> int:
+        """
+        Args:
+            roll_index (int): Roll turn in a game.
+
+        Returns:
+            int: The sum of pins knock down in a frame.
+        """
+        return self.__rolls[roll_index] + self.__rolls[roll_index + 1]
--- test_bowling_game.py
+++ test_bowling_game.py
@@ -3,27 +3,76 @@ from controllers.game import Game
 
 class TestBowlingGame:
 
-    def seutp_class(self):
-        print("Acá todo comienza")
-
     def setup_method(self):
+        """
+        Setup method for a Game instance.
+        This instance is for each test.
+        """
         self.g = Game()
 
-    def roll_many(self, frames, pins):
+    def roll_many(self, frames: int, pins: int):
+        """
+        Method for roll many pins in various frames.
+
+        Args:
+            frames (int): frames to play
+            pins (int): Pins knock down in frames,
+            the same amount of pins in every frame.
+        """
         for i in range(frames):
             self.g.roll(pins)
 
     def test_gutter_game(self):
+        """
+        Test for don't knock down any pins in any frame.
+        """
         self.roll_many(20, 0)
         assert 0 == self.g.score()
 
     def test_all_ones(self):
+        """
+        Test for knock down one pin in each frame
+        """
         self.roll_many(20, 1)
         assert 20 == self.g.score()
-
+    
     def test_one_spare(self):
-        self.g.roll(5)
-        self.g.roll(5) #spare
+        """
+        Test for one spare in a game
+        """
+        self.roll_spare()
         self.g.roll(3)
         self.roll_many(17, 0)
         assert 16 == self.g.score()
+
+    def roll_spare(self):
+        """
+        Spare roll method. The player knocks all 
+        10 pins in two rolls.
+        """
+        self.g.roll(5)
+        self.g.roll(5)
+
+    def test_one_strike(self):
+        """
+        Test for one strike in the game.
+        """
+        self.roll_strike() 
+        self.g.roll(3)
+        self.g.roll(4)
+        self.roll_many(16, 0)
+        assert 24 == self.g.score()
+
+    def roll_strike(self):
+        """
+        Strike roll method. The player knocks down all 
+        10 pins on his first roll.
+        """
+        self.g.roll(10)
+
+    def test_perfect_game(self):
+        """
+        Test for a perfect game. Strike roll on all frames.
+        """
+        self.roll_many(20, 10)
+        assert 300 == self.g.score()
